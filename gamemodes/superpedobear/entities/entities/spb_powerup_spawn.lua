fac78b17d5949906e4b553465c4d4c0cf2ffeb9a
VictorienXP <VictorienXP@Xperidia.com>
2017-08-11 18:55:55 +0200

    Added some IO to spb_powerup_spawn

--- gamemodes/superpedobear/entities/entities/spb_powerup_spawn.lua
+++ gamemodes/superpedobear/entities/entities/spb_powerup_spawn.lua
@@ -15,18 +15,34 @@ end
 function ENT:KeyValue(k, v)
 	if k == "PowerUP" then
 		self.ForcedPowerUP = v
+	elseif k == "RespawnTime" then
+		self.RespawnTime = v
+	end
+	if string.Left(k, 2) == "On" then
+		self:StoreOutput(k, v)
 	end
 end
 
-if SERVER then
-	function ENT:Think()
-		if !self.SpawnedPowerUP then
-			self.SpawnedPowerUP = GAMEMODE:CreatePowerUP(self, self.ForcedPowerUP)
-		elseif !IsValid(self.SpawnedPowerUP) and !self.Wait then
-			self.Wait = CurTime() + 30
-		elseif self.Wait and self.Wait < CurTime() then
-			self.SpawnedPowerUP = GAMEMODE:CreatePowerUP(self, self.ForcedPowerUP, true)
-			self.Wait = nil
-		end
+function ENT:Think()
+	if !SERVER or self.Disabled then return end
+	if !self.SpawnedPowerUP then
+		self.SpawnedPowerUP = GAMEMODE:CreatePowerUP(self, self.ForcedPowerUP)
+	elseif !IsValid(self.SpawnedPowerUP) and !self.Wait then
+		self.Wait = CurTime() + (self.RespawnTime or 30)
+		self:TriggerOutput("OnPickup")
+	elseif self.Wait and self.Wait < CurTime() then
+		self.SpawnedPowerUP = GAMEMODE:CreatePowerUP(self, self.ForcedPowerUP, true)
+		self.Wait = nil
 	end
+end
+
+function ENT:AcceptInput(name, activator, caller, data)
+	if name == "ForceRespawn" and !IsValid(self.SpawnedPowerUP) then
+		self.Wait = 0
+	elseif name == "Disable" then
+		self.Disabled = true
+	elseif name == "Enable" then
+		self.Disabled = false
+	end
+	return true
 end

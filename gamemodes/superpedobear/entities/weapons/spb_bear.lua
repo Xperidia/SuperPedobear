8dda44b218c78b0b4204464621957befda2f5a31
VictorienXP <VictorienXP@Xperidia.com>
2017-08-14 15:08:20 +0200

    Reworked the bear swep a little bit

--- gamemodes/superpedobear/entities/weapons/spb_bear.lua
+++ gamemodes/superpedobear/entities/weapons/spb_bear.lua
@@ -82,14 +82,8 @@ end
 function SWEP:Think()
 	if SERVER and GAMEMODE.Vars.Round.Start then
 		local attacker = self.Owner
-		for k, v in pairs (ents.FindInSphere(self.Owner:GetPos() + Vector(0,0,16), 26)) do
-			if IsValid(v) and ((v:IsPlayer() and v:Alive() and v:Team() == TEAM_HIDING and v != self.Owner) or v:GetClass() == "spb_dummy") and v:Health() > 0 then
-				if !IsValid(attacker) then attacker = self end
-				self:GangBang(v, attacker)
-			end
-		end
-		for k, v in pairs (ents.FindInSphere(self.Owner:GetPos() + (self.Owner:GetViewOffset() or Vector(0,0,32)), 26)) do
-			if IsValid(v) and ((v:IsPlayer() and v:Alive() and v:Team() == TEAM_HIDING and v != self.Owner) or v:GetClass() == "spb_dummy") and v:Health() > 0 then
+		for k, v in pairs(ents.FindInSphere(self.Owner:GetPos() + Vector(0, 0, 32), 32.2)) do
+			if IsValid(v) and ((v:IsPlayer() and v:Alive() and v:Team() == TEAM_HIDING and self.Owner:IsLineOfSightClear(v)) or v:GetClass() == "spb_dummy") then
 				if !IsValid(attacker) then attacker = self end
 				self:GangBang(v, attacker)
 			end

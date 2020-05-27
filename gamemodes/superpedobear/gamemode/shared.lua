8d0895d9c4812d0e2a48d47bb52ffbf56932c88c
VictorienXP <VictorienXP@Xperidia.com>
2020-05-27 11:37:16 +0200

    Update shared.lua

--- gamemodes/superpedobear/gamemode/shared.lua
+++ gamemodes/superpedobear/gamemode/shared.lua
@@ -14,7 +14,7 @@ GM.Name 		= "Super Pedobear"
 GM.Author 		= "VictorienXP@Xperidia"
 GM.Website 		= "steamcommunity.com/sharedfiles/filedetails/?id=628449407"
 GM.Version 		= v"0.1.2"
-GM.VersionDate 	= 200503
+GM.VersionDate 	= 200527
 GM.TeamBased 	= true
 
 TEAM_HIDING	= 1

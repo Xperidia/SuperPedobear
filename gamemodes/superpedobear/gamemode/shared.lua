2cf057ee9393d6f1eb3eb080efb4cc6f610c35b9
VictorienXP <VictorienXP@Xperidia.com>
2020-06-21 03:18:12 +0200

    Update shared.lua

--- gamemodes/superpedobear/gamemode/shared.lua
+++ gamemodes/superpedobear/gamemode/shared.lua
@@ -13,7 +13,7 @@ local v = include("semver.lua")
 GM.Name 		= "Super Pedobear"
 GM.Author 		= "VictorienXP@Xperidia"
 GM.Website 		= "steamcommunity.com/sharedfiles/filedetails/?id=628449407"
-GM.Version 		= v"0.2.3"
+GM.Version 		= v"0.3.0"
 GM.VersionDate 	= 200621
 GM.TeamBased 	= true
 GM.IsSPBDerived	= true
